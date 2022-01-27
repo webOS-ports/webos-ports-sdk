d2ba9c2130b01854ab5fd0887a153853212bc441
codepoet80 <jon.wise@jonandnic.com>
2022-01-27 16:16:44 -0500

    new install command for devices, per Herrie. Support remove.

--- scripts/lune-install.sh
+++ scripts/lune-install.sh
@@ -13,27 +13,63 @@ if [ "$1" = "" ]; then
     exit
 fi
 IPK=$1
+DEVICE=1
 
 # Make sure there's a device to run on
 devfound=false
 adb get-state 1>/dev/null 2>&1 && devfound=true || devfound=false
 if [ "$devfound" = "false" ]; then
-    echo lune-install: no devices found via adb
+    echo lune-install: no devices found via adb, assuming emulator
+    DEVICE=0
+fi
+
+# Figure all the names we need
+ipkfile=$(basename "$IPK")
+ipkname="$(echo $ipkfile | cut -d'_' -f1)"
+
+# If they asked to remove an app, do that instead of everything else
+if [ "$2" = "-r" ]; then
+    echo removing package $ipkname
+    # new command?
+    command="/usr/bin/luna-send -n 10 -f luna://com.webos.appInstallService/remove '{ \"id\": \"$ipkname\", \"subscribe\": true }'"
+    if [ $DEVICE -eq 1 ]; then
+        adb shell $command
+    else
+        # emulator currently still uses original command
+        command="/usr/bin/luna-send -n 10 -f luna://com.palm.appinstaller/remove '{ \"packageName\": \"$ipkname\", \"subscribe\": true }'"
+        ssh root@localhost -p 5522 $command
+    fi
     exit
 fi
 
-# Figure all the names we need and tidy up
-ipkfile=$(basename "$IPK")
-ipkname="$(echo $ipkfile | cut -d'_' -f1)"
-adb shell "rm /tmp/*.ipk 2>null"
+# Tidy up
+command="rm /tmp/*.ipk 2>null"
+if [ $DEVICE -eq 1 ]; then
+    adb shell $command
+else
+    ssh root@localhost -p 5522 $command
+fi
 
 # Push and install the app
 echo pushing package $IPK
-adb push $IPK /tmp
+command="$IPK"
+if [ $DEVICE -eq 1 ]; then
+    adb push $command /tmp
+else
+    scp -P 5522 $command root@localhost:/tmp
+fi
 echo
-echo (re)installing $ipkname
+
+echo re/installing $ipkname
 # To install a System app:
 #   adb shell "opkg install --force-reinstall --force-downgrade /tmp/$ipkfile && rm /tmp/*.ipk"
-adb shell "/usr/bin/luna-send -n 10 -f luna://com.palm.appinstaller/remove '{ \"packageName\": \"$IPKNAME\", \"subscribe\": true }'" 2>&1
-adb shell "/usr/bin/luna-send -n 6 luna://com.palm.appinstaller/installNoVerify '{\"subscribe\":true, \"target\": \"/tmp/$ipkfile\"}'"
+# new command, per Herrie
+command="/usr/bin/luna-send -n 1 luna://com.webos.appInstallService/install  '{\"subscribe\":true, \"id\": \"$ipkname\", \"ipkUrl\": \"/tmp/$ipkfile\"}'"
+if [ $DEVICE -eq 1 ]; then
+    adb shell $command
+else
+    # emulator currently still uses original command
+    command="/usr/bin/luna-send -n 6 luna://com.palm.appinstaller/installNoVerify '{\"subscribe\":true, \"target\": \"/tmp/$ipkfile\"}'"
+    ssh root@localhost -p 5522 $command
+fi
 sleep 1
